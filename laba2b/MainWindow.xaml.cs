32dd29bf1141e41ace19868af0bdbd7a84367a4b
Andytemp <andy844551@gmail.com>
2020-10-19 15:43:14 +0300

    Ported Menu and apply button, no testing

--- laba2b/MainWindow.xaml.cs
+++ laba2b/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace laba2b
     {
         System.Drawing.Image ImageBuffer;
         System.Drawing.Image SecondBuffer;
+        String FileName;
         System.Drawing.Color CurrentColor = System.Drawing.Color.Black;
         List<System.Drawing.Point> DrawingNodes = new List<System.Drawing.Point>();
         bool IsDrawing = false;
@@ -59,6 +60,31 @@ namespace laba2b
         }
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
+
+        }
+
+        private void MenuItem_SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            SecondBuffer.Save(FileName);
+            ImageBuffer = (System.Drawing.Image)SecondBuffer.Clone();
+            //ResetControls();
+        }
+
+        private void MenuItem_SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog SaveFileDialog_Save = new SaveFileDialog();
+            if (SaveFileDialog_Save.ShowDialog() == true && SaveFileDialog_Save.FileName.Length > 0)
+            {
+                SecondBuffer.Save(SaveFileDialog_Save.FileName);
+            }
+            FileName = SaveFileDialog_Save.FileName;
+            ImageBuffer = (System.Drawing.Image)SecondBuffer.Clone();
+            Window_Main.Title = SaveFileDialog_Save.FileName + " - Image Editor";
+            //ResetControls();
+        }
+
+        private void MenuItem_OpenFile_Click(object sender, RoutedEventArgs e)
+        {
             OpenFileDialog OpenFileDialog_Open = new OpenFileDialog();
             if (OpenFileDialog_Open.ShowDialog() == true && OpenFileDialog_Open.FileName.Length > 0)
             {
@@ -69,8 +95,15 @@ namespace laba2b
                     Display(System.Drawing.Image.FromStream(fs));
                 }
                 Window_Main.Title = OpenFileDialog_Open.FileName + " - Image Editor";
+                FileName = OpenFileDialog_Open.FileName;
                 Border_Tools.IsEnabled = true;
             }
+        }
+
+        private void Button_Apply_Click(object sender, RoutedEventArgs e)
+        {
+            ImageBuffer = (System.Drawing.Image)SecondBuffer.Clone();
+            //ResetControls();
         }
     }
 }
